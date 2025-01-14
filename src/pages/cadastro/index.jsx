8f8e95ba8e9ae60c6dd967f47e0d14e6aa3bde17
Daniel A. Gonçalves de Oliveira <86572380+DanielOliveira2@users.noreply.github.com>
2025-01-14 20:42:26 -0300

    implementando tela de login e cadastro

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -3,7 +3,48 @@ import "./style.css"
 export default function Cadastro(){
   return(
     <>
-    
+      <div className="cadastro-container">
+      <div className="logo">
+        <img src="../src/assets/logo.png" alt="Taste Italy Logo" />
+      </div>
+      <form className="cadastro-form">
+        <label htmlFor="nome">Nome:</label>
+        <input
+          type="text"
+          id="nome"
+          placeholder="Digite seu nome"
+          required
+        />
+
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          required
+        />
+
+        <label htmlFor="senha">Senha:</label>
+        <input
+          type="password"
+          id="senha"
+          placeholder="Digite sua senha"
+          required
+        />
+
+        <label htmlFor="confirmarSenha">Confirmar Senha:</label>
+        <input
+          type="password"
+          id="confirmarSenha"
+          placeholder="Confirme sua senha"
+          required
+        />
+
+        <button type="submit" className="btn registrar">
+          Registrar
+        </button>
+      </form>
+    </div>
     </>
   )
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,8 +3,25 @@ import "./style.css"
 export default function Login(){
   return(
     <>
-      <h1>teste</h1>
-      <p>kdncirhvbrjvb</p>
+      <div className="login-container">
+      <div className="logo">
+        <img src="../src/assets/logo.png" alt="Taste Italy Logo" />
+      </div>
+      <form className="login-form">
+        <label htmlFor="email">E-mail:</label>
+        <input type="email" id="email" placeholder="Digite seu e-mail" required />
+        <label htmlFor="password">Senha:</label>
+        <input type="password" id="password" placeholder="Digite sua senha" required />
+        <div className="button-group">
+          <button type="button" className="btn register">
+            Registrar
+          </button>
+          <button type="submit" className="btn login">
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
     </>
   )
 }
