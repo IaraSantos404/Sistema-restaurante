b49a9e600473dd982aea6d10e3c4858ce00542f8
iara <iarasantosdev404@gmail.com>
2025-01-17 17:38:59 -0300

    ajuste no menu

--- src/components/cards-menu/index.jsx
+++ src/components/cards-menu/index.jsx
@@ -1,15 +1,16 @@
-import "./style.css"
-// import Heart from "../../assets/images/heart.svg"
+import "./style.css";
+// import Heart from "../../assets/images/heart.svg";
 import PropTypes from "prop-types";
 
 Card.propTypes = {
   nome: PropTypes.string.isRequired,
-  preco: PropTypes.number.isRequired, 
-  descricao: PropTypes.string,        
-  img: PropTypes.string.isRequired    
+  preco: PropTypes.number.isRequired,
+  descricao: PropTypes.string,
+  img: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
-export default function Card({nome, preco, descricao, img}){
+export default function Card({ nome, preco, descricao, img, onClick }) {
   const formatarPreco = (valor) => {
     return new Intl.NumberFormat("pt-BR", {
       style: "currency",
@@ -17,29 +18,25 @@ export default function Card({nome, preco, descricao, img}){
     }).format(valor);
   };
 
-  return(
-    <>
-      <div id="cards-container">
-        <div className="box">
-          <div className="box-img">
-            {/* <div className="heart">
-              <img src={Heart} alt="" />
-            </div> */}
-            
-            <img className="img-comida" src={img} alt="" />
+  return (
+    <div id="cards-container" onClick={onClick} style={{ cursor: "pointer" }}>
+      <div className="box">
+        <div className="box-img">
+          {/* <div className="heart">
+            <img src={Heart} alt="" />
+          </div> */}
+          <img className="img-comida" src={img} alt={nome} />
+        </div>
+        <div className="box-text">
+          <div className="div1">
+            <p className="nome">{nome}</p>
+            <p className="preco">{formatarPreco(preco)}</p>
           </div>
-          <div className="box-text">
-            <div className="div1">
-              <p className="nome">{nome}</p>
-              <p className="preco">{formatarPreco(preco)}</p>
-            </div>
-            <div className="div2">
-              <p className="descricao">{descricao}</p>
-            </div>
-            
+          <div className="div2">
+            <p className="descricao">{descricao}</p>
           </div>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,59 +1,94 @@
-import "./style.css"
-import Card from "../../components/cards-menu"
-import Pizza1 from "../../assets/images/pizza1.png"
-import Pizza2 from "../../assets/images/pizza2.svg"
-import Salada from "../../assets/images/salada.png"
-import Macarrao from "../../assets/images/macarrao.png"
-import Drink1 from "../../assets/images/drink1.png"
-import Drink2 from "../../assets/images/drink2.png"
+import { useState } from "react";
+import "./style.css";
+import Card from "../../components/cards-menu";
+import Pizza1 from "../../assets/images/pizza1.png";
+import Pizza2 from "../../assets/images/pizza2.svg";
+import Salada from "../../assets/images/salada.png";
+import Macarrao from "../../assets/images/macarrao.png";
+import Drink1 from "../../assets/images/drink1.png";
+import Drink2 from "../../assets/images/drink2.png";
 
-export default function Menu(){
-  return(
+export default function Menu() {
+  const [selectedCategory, setSelectedCategory] = useState("Tudo");
+
+  const items = [
+    {
+      nome: "Pizza Vegetariana",
+      preco: 35,
+      descricao: "Cobertura de vegetais frescos, queijo derretido e molho de tomate especial.",
+      img: Pizza1,
+      categoria: "Pizzas",
+    },
+    {
+      nome: "Penne ao Pesto",
+      preco: 15.5,
+      descricao: "Massa penne com molho pesto de manjericão, tomatinhos cereja e parmesão.",
+      img: Salada,
+      categoria: "Entradas",
+    },
+    {
+      nome: "Macarrão Alfredo",
+      preco: 23,
+      descricao: "Massa fettuccine ao molho cremoso de queijo parmesão e manteiga.",
+      img: Macarrao,
+      categoria: "Entradas",
+    },
+    {
+      nome: "Água de Limão",
+      preco: 7.5,
+      descricao: "Bebida leve com rodelas de limão, folhas de hortelã e frutas vermelhas.",
+      img: Drink1,
+      categoria: "Drinks",
+    },
+    {
+      nome: "Pizza Margherita",
+      preco: 28,
+      descricao: "Cobertura de molho de tomate, queijo mozzarella fresco e manjericão.",
+      img: Pizza2,
+      categoria: "Pizzas",
+    },
+    {
+      nome: "Drink Tropical",
+      preco: 15,
+      descricao: "Coquetel com toque de hortelã, limão e leve dulçor tropical.",
+      img: Drink2,
+      categoria: "Drinks",
+    },
+  ];
+
+  const filteredItems =
+    selectedCategory === "Tudo"
+      ? items
+      : items.filter((item) => item.categoria === selectedCategory);
+
+  return (
     <>
       <div id="menu">
         <h1>Nosso menu</h1>
-        
+
         <div className="opcoes-menu">
-          <p className="selecionado">Tudo</p>
-          <p>Drinks</p>
-          <p>Entradas</p>
-          <p>Pizzas</p>
+          {["Tudo", "Drinks", "Entradas", "Pizzas"].map((categoria) => (
+            <p
+              key={categoria}
+              className={selectedCategory === categoria ? "selecionado" : ""}
+              onClick={() => setSelectedCategory(categoria)}
+            >
+              {categoria}
+            </p>
+          ))}
         </div>
         <div className="div-cards">
-          <Card 
-            nome={"Pizza Vegetariana"}
-            preco={35}
-            descricao={"Cobertura de vegetais frescos, queijo derretido e molho de tomate especial."}
-            img={Pizza1}/>
-          <Card 
-            nome={"Penne ao Pesto"}
-            preco={15.50}
-            descricao={"Massa penne com molho pesto de manjericão, tomatinhos cereja e parmesão."}
-            img={Salada}/>
-          <Card 
-            nome={"Macarrão Alfredo"}
-            preco={23}
-            descricao={"Massa fettuccine ao molho cremoso de queijo parmesão e manteiga."}
-            img={Macarrao}/>
-          <Card 
-            nome={"Água de Limão"}
-            preco={7.5}
-            descricao={"Bebida leve com rodelas de limão, folhas de hortelã e frutas vermelhas."}
-            img={Drink1}/>
-          <Card 
-            nome={"Pizza Margherita"}
-            preco={28}
-            descricao={"Cobertura de molho de tomate, queijo mozzarella fresco e manjericão."}
-            img={Pizza2}/>
-          <Card 
-            nome={"Drink Tropical"}
-            preco={15}
-            descricao={"Coquetel com toque de hortelã, limão e leve dulçor tropical."}
-            img={Drink2}/>
-          
+          {filteredItems.map((item, index) => (
+            <Card
+              key={index}
+              nome={item.nome}
+              preco={item.preco}
+              descricao={item.descricao}
+              img={item.img}
+            />
+          ))}
         </div>
       </div>
-    
     </>
-  )
+  );
 }
